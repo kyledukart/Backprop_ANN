e51dac390cba75a95757fdd4ec2a2d91002c2b64
Kyle Dukart <46544456+kyledukart@users.noreply.github.com>
2020-02-23 13:17:01 -0800

    Add files via upload

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 % main function for neural network with backpropagation
-function output = main(S1, S2, learningRate)
+function output = main(S1, S2, learningRate, epochs)
 
 % get input patterns
 P = inputPatterns();  % input P is a 3 x 30 matrix
@@ -16,28 +16,34 @@ W2 = zeros(S2,S1);  % W2 is a weight matrix for layer 2, with a size of S2 x S1
 % set answers
 t = [1,0,0;0,1,0;0,0,1];   % t is the given answers matrix, with a size of S2 x 3
 
-%% loop for each input in P (1 epoch)
-for i = 1:3
-%% forward compute
-[n1,a1] = neuronLayer(W1,P(:,i),B1);  % a1 is the results of layer 1, with a size of S1 x 1
-[n2,a2] = neuronLayer(W2,a1,B2);  % a2 is the results of computation, with a size of S2 x 1
+%% loop for each epoch
+for j = 1:epochs
 
-%% backpropagation
-% compute output layer sensitivities
-s2 = -2 * (t(:,i) - a2) .* logsigdot(n2);
+    %% loop for each input in P (1 epoch)
+    for i = 1:3
+        %% forward compute
+        [n1,a1] = neuronLayer(W1,P(:,i),B1);  % a1 is the results of layer 1, with a size of S1 x 1
+        [n2,a2] = neuronLayer(W2,a1,B2);  % a2 is the results of computation, with a size of S2 x 1
 
-% propogate sensitivities backwards
-s1 = logsigdot(n1)' * W2' * s2 % TODO problems here, wrong dimensions, questionable transpose
+        %% backpropagation
+        % compute output layer sensitivities
+        s2 = -2 * (t(:,i) - a2) .* logsigdot(n2);
 
-% update W2 and B2
-% W2 = W2 - learningRate * s2 * a1';
-% B2 = B2 - learningRate * s2;
+        % propogate sensitivities backwards
+        s1 = logsigdot(n1) .* W2' * s2; % TODO problems here, questionable transpose and dot product
 
-% update W1 and B1
-% W1 = W1 - learningRate * s1 * P';
-% B1 = B1 - learningRate * s1;
+        % update W2 and B2
+        W2 = W2 - learningRate * s2 * a1';
+        B2 = B2 - learningRate * s2;
 
-end  % end loop through input values i (1 epoch)
+        % update W1 and B1
+        W1 = W1 - learningRate * s1 * P(:,i)';
+        B1 = B1 - learningRate * s1;
+
+    end  % end loop through input values i (1 epoch)
+    
+    % save percent error each epoch for graphing purposes
+end  % end loop through epochs (j)
 %% graph output
 
 
