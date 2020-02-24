1cb9f02d15c3bfcb80faf3c5bc97e458726bcac3
Vlad <vladnet@uw.edu>
2020-02-23 19:02:54 -0800

    Fixed part1 output

--- inputPatterns.m
+++ inputPatterns.m
@@ -1,8 +1,10 @@
-function P = inputPatterns()
-% creates vectors corresponding to each of the numbers 0-2 in a 5x3 matrix,
-% reading each column one at a time.
-zero = [-1,1,1,1,1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,1,-1,1,1,1,1,-1];
-one = [-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
-two = [1,-1,-1,-1,-1,-1,1,-1,-1,1,1,1,1,-1,-1,1,-1,1,-1,1,1,-1,-1,1,-1,-1,-1,-1,-1,1];
-P = [zero(:),one(:),two(:)];
+function [P, T] = inputPatterns()
+    % creates vectors corresponding to each of the numbers 0-2 in a 5x3 matrix,
+    % reading each column one at a time.
+    zero = [-1,1,1,1,1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,1,-1,1,1,1,1,-1];
+    one = [-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
+    two = [1,-1,-1,-1,-1,-1,1,-1,-1,1,1,1,1,-1,-1,1,-1,1,-1,1,1,-1,-1,1,-1,-1,-1,-1,-1,1];
+    P = [zero' one' two'];
+    
+    T = [1,0,0;0,1,0;0,0,1];
 end
--- main.m
+++ main.m
@@ -2,34 +2,7 @@
 function output = main(S1, S2, learningRate, epochs)
 
 %% Part 1
-% get input patterns
-P = inputPatterns();  % input P is a 3 x 30 matrix
-T = [1,0,0;0,1,0;0,0,1];   % t is the given answers matrix, with a size of S2 x 3
-
-[W1, B1, W2, B2, MSE1] = trainNetwork(P, T, [S1 S2], learningRate, epochs);
-
-W1
-B1
-W2
-B2
-
-P = addNoise(inputPatterns(),4);
-
-[W1, B1, W2, B2, MSE2] = trainNetwork(P, T, [S1 S2], learningRate, epochs);
-W1
-B1
-W2
-B2
-
-P = addNoise(inputPatterns(),8);
-
-[W1, B1, W2, B2, MSE3] = trainNetwork(P, T, [S1 S2], learningRate, epochs);
-W1
-B1
-W2
-B2
-
-graphPart1(MSE1, MSE2, MSE3);
+part1([S1 S2], learningRate, epochs);
 
 %% Part 2
 
